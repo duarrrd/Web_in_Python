f06efd64b83f475917aa8925b89e292fe3357ab3
duarrrd <duarrrd88@gmail.com>
2023-10-11 15:56:42 +0300

    LAB 2 task-2 DONE Cookies have been implemented, in particular a counter of the number of forms filled out by the user. Also implement a button that deletes all cookies.

--- lab2/cgi-bin/form_handler.py
+++ lab2/cgi-bin/form_handler.py
@@ -1,6 +1,27 @@
 import cgi
+import cgitb
+import os
+
+cgitb.enable()
 
 form = cgi.FieldStorage()
+
+cookie_str = os.environ.get('HTTP_COOKIE')
+cookie_dict = {}
+if cookie_str:
+    cookie_list = cookie_str.split('; ')
+    for cookie in cookie_list:
+        key, value = cookie.split('=')
+        cookie_dict[key] = value
+
+count = int(cookie_dict.get('form_count', 0))
+
+if 'delete_cookies' in form:
+    count = 0
+    print("Cookies deleted")
+
+if form.getvalue('submit'):
+    count += 1
 
 name = form.getvalue("name")
 email = form.getvalue("email")
@@ -8,7 +29,9 @@ gender = form.getvalue("gender")
 interests = form.getlist("interests")
 country = form.getvalue("country")
 
-print("Content-type: text/html\n")
+print("Content-type: text/html")
+print("Set-Cookie: form_count={};".format(count))
+print() 
 
 print("<html>")
 print("<head>")
@@ -21,5 +44,13 @@ print("<p><strong>Email:</strong> {}</p>".format(email))
 print("<p><strong>Gender:</strong> {}</p>".format(gender))
 print("<p><strong>Interests:</strong> {}</p>".format(", ".join(interests)))
 print("<p><strong>Country:</strong> {}</p>".format(country))
+print("<p>Number of forms submitted: {}</p>".format(count))
+print('<p><a href="../index.html">Fill out a new form</a></p>')
+print('<form method="POST">')
+print('<input type="submit" name="submit" value="Submit Form">')
+print('</form>')
+print('<form method="POST">')
+print('<input type="submit" name="delete_cookies" value="Delete Cookies">')
+print('</form>')
 print("</body>")
 print("</html>")
