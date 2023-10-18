54991486003b2ff9c35529f9b46ae2b4993d6d99
duarrrd <duarrrd88@gmail.com>
2023-10-18 19:43:13 +0300

    LAB 3 footer

--- lab3/app.py
+++ lab3/app.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():  # put application's code here
-    return render_template('main.html')
+def main():
+    OS = os.environ['OS']
+    u_agent = request.user_agent
+    time = datetime.now().strftime("%H:%M")
+    return render_template('main.html', u_agent=u_agent, time=time, OS=OS)
 
 @app.route('/l_and_s')
-def l_and_s():  # put application's code here
+def l_and_s():
     return render_template('L&S.html')
 
 @app.route('/skills')
-def skills():  # put application's code here
+def skills():
     return render_template('skills.html')
 
 
