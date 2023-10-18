5d761cccecd9908815fa5d346eaf0bda4ae425f3
duarrrd <duarrrd88@gmail.com>
2023-10-18 19:25:48 +0300

    LAB 3 menu, certif. dynamic titles And many other things

--- lab3/app.py
+++ lab3/app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def main():  # put application's code here
     return render_template('main.html')
 
+@app.route('/l_and_s')
+def l_and_s():  # put application's code here
+    return render_template('L&S.html')
+
+@app.route('/skills')
+def skills():  # put application's code here
+    return render_template('skills.html')
+
 
 if __name__ == '__main__':
     app.run()
