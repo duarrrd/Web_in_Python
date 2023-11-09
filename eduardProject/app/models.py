e09aaac02655f486b6789fc45f89f606a1d04f86
duarrrd <duarrrd88@gmail.com>
2023-11-09 12:53:18 +0200

    {LAB6} upd models.py, add todo

--- eduardProject/app/models.py
+++ eduardProject/app/models.py
@@ -4,3 +4,8 @@ class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     comment = db.Column(db.Text, nullable=False)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.Boolean, default=False)
