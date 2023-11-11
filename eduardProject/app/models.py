b953120536ae1ac5d5f7336bd5d7e98e2a24c680
duarrrd <duarrrd88@gmail.com>
2023-11-11 15:27:33 +0200

    {LAB7} t3 user model

--- eduardProject/app/models.py
+++ eduardProject/app/models.py
@@ -9,3 +9,13 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(100), nullable=False)
     status = db.Column(db.Boolean, default=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Colimn(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20),  nullable=False, default="generic.jpg")
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
