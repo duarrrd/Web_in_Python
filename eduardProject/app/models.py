15797a1643563aae00bcf2ec82695ba25db4594c
duarrrd <duarrrd88@gmail.com>
2023-11-11 17:53:38 +0200

    {LAB7} t4 DB change

--- eduardProject/app/models.py
+++ eduardProject/app/models.py
@@ -13,7 +13,7 @@ class Todo(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Colimn(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20),  nullable=False, default="generic.jpg")
     password = db.Column(db.String(60), nullable=False)
 
