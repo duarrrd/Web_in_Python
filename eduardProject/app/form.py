def5735f93bd7bde418b69e2f97ef58543c21d50
duarrrd <duarrrd88@gmail.com>
2023-11-11 18:31:16 +0200

    {LAB7} t5 DB login and registration forms

--- eduardProject/app/form.py
+++ eduardProject/app/form.py
@@ -1,13 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    name = StringField(label='User name', validators=[DataRequired("Це поле обов'язкове")])
-    password = PasswordField(label='Password', validators=[DataRequired("Це поле обов'язкове"),
-                Length(min=4, max=10, message="Повинно бути від 4 до 10 символів")])
+    email = StringField(label='Email', validators=[DataRequired("Це поле обов'язкове"), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired("Це поле обов'язкове")])
     remember = BooleanField(label="Запам'ятати мене")
     submit = SubmitField(label="Ввійти")
+
+class RegistrationForm(FlaskForm):
+    username = StringField(label='User name', validators=[DataRequired("Це поле обов'язкове"),
+            Length(min=4, max=14, message="Від 4 до 14")
+        ])
+    email = StringField(label='Email', validators=[DataRequired("Це поле обов'язкове"), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired("Це поле обов'язкове"),
+            Length(min=7, message="Mінімум 7 символів")
+        ])
+    confirm_password = PasswordField(label='Password', validators=[DataRequired("Це поле обов'язкове")])
+    submit = SubmitField(label="Зареєструватись")
 
 class ChangePasswordForm(FlaskForm):
     old_password = PasswordField(label='Old password', validators=[DataRequired("Це поле обов'язкове"),
