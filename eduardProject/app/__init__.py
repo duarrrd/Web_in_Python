5358fbbce782d801aa9e25607b50f53b727ec298
duarrrd <duarrrd88@gmail.com>
2023-11-12 15:41:41 +0200

    {LAB8}-t1

--- eduardProject/app/__init__.py
+++ eduardProject/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
 
 from . import views
