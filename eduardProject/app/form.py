a2798500ba6af83cb31905242dac8ffef0da80a0
duarrrd <duarrrd88@gmail.com>
2023-11-13 11:41:12 +0200

    {Lab9}-fix2

--- eduardProject/app/form.py
+++ eduardProject/app/form.py
@@ -65,13 +65,6 @@ class UpdateAccountForm(FlaskForm):
 
     submit = SubmitField(label="Update")
 
-    def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('Email already registered')
-
-    def validate_username(self, field):
-        if User.query.filter_by(username=field.data).first():
-            raise ValidationError("Username already in use")
 
     def validate_username(self, username):
         if username.data != current_user.username:
