cba27594d5e19371ff69c3316ad51a912ba8a48f
duarrrd <duarrrd88@gmail.com>
2023-12-20 18:30:22 +0200

    {DEPLOY} PostgreSQL fix4

--- eduardProject/config.py
+++ eduardProject/config.py
@@ -5,17 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = 'your_secret_key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    if os.getenv('DATABASE_URL'):
-        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
-    else:
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
 
 class DevConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
 
 class ProdConfig(Config):
-    pass  # No need to set SQLALCHEMY_DATABASE_URI here if it's the same as Config
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestConfig(Config):
     DEBUG = False
